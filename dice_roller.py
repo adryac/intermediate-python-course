b3e96eabe56035fa0596f825c2a4d689d29a5dbc
adryac <adryac@users.noreply.github.com>
2020-02-28 23:00:11 +0100

    "Tutorial complete"

--- dice_roller.py
+++ dice_roller.py
@@ -5,8 +5,13 @@ def main():
   for i in range(0,dice_rolls):
       roll=random.randint(1,6)
       dice_sum+=roll
-      print(f'You rolled a {roll}')
-  print(f'you have rolled a total {dice_sum}')
+      if roll==1:
+          print(f'You rolled a {roll}! Critical Fail')
+      elif roll==6:
+          print(f'You rolled a {roll}! Critical Succes!')
+      else:
+          print(f'You rolled a {roll}')
+  print(f'You have rolled a total {dice_sum}')
 
 if __name__== "__main__":
   main()
